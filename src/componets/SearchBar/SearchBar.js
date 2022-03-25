40bb7b2f99bdecc3b8a5189457d22ebee1aadf4d
joseungjin <jn5947@naver.com>
2022-03-25 23:22:31 +0900

    css remove

--- src/componets/SearchBar/SearchBar.js
+++ src/componets/SearchBar/SearchBar.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
-import Main from "../Main/Main";
-import {Input} from 'antd';
-import 'antd/dist/antd.min.css';
 
 function SearchBar({results, updateField, keyword}) {
-  const { Search } = Input;
+
   return (
       <div >
-          <input
+          <input style={{width:'100%', height:'100%'}} 
           placeholder="Search"
           onChange={e => updateField("keyword", e.target.value)}
           />
       </div>
       
-  //<Search  placeholder="input search text"  enterButton onChange={e => updateField("keyword", e.target.value)} />
+
  
   )
   
