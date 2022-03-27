415bf9437b989be4beead667503975c61a1b0145
joseungjin <jn5947@naver.com>
2022-03-27 23:30:52 +0900

    css add

--- src/componets/SearchBar/SearchBar.js
+++ src/componets/SearchBar/SearchBar.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-
+import '../../componets/css/SearchBar.css'
 function SearchBar({results, updateField, keyword}) {
 
   return (
-      <div >
-          <input 
+      <div className="auto">
+       <button
+        className={`cancel-btn active`}
+      >
+        x
+      </button>
+          <input
+          className="search-bar"
           placeholder="Search"
+          value={keyword}
           onChange={e => updateField("keyword", e.target.value)}
-          />
+        />
+      
       </div>
 
  
