75de7633d9847cb0349e6044950028edf8598e23
joseungjin <jn5947@naver.com>
2022-04-13 23:34:54 +0900

    modify

--- src/componets/SearchBar/SearchBar.js
+++ src/componets/SearchBar/SearchBar.js
@@ -22,7 +22,7 @@ var renderResults = Medicine.map((name,index) => {
   return (
     
       <div className="auto">
-        <span style={{fontSize:"40px", width:"blod"}}>About</span>
+        <span style={{fontSize:"40px", width:"bold"}}>About</span>
        <button
         className={`cancel-btn active`}
       >
