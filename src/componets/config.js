74eeabea051612e8f9744fb69188b7eaf7aebcc5
JoSeungJin <82432906+joseungjin@users.noreply.github.com>
2022-05-15 17:21:14 +0900

    Delete config.js
    default

--- src/componets/config.js
+++
@@ -1,2 +0,0 @@
-//export const API_URL ="http://apis.data.go.kr/1471000/MdcinGrnIdntfcInfoService01/getMdcinGrnIdntfcInfoList01?serviceKey=PkKuhTTMfvkADH3addD1sS6cR5lvf43sgSkgj9XOpwiJ3%2BDj62msgsUOmAS38Ae4kwOqJNUwsLcSLObmwejzKA%3D%3D&item_name=";
-export const API_URL="https://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList?serviceKey=PkKuhTTMfvkADH3addD1sS6cR5lvf43sgSkgj9XOpwiJ3%2BDj62msgsUOmAS38Ae4kwOqJNUwsLcSLObmwejzKA%3D%3D&itemName="
