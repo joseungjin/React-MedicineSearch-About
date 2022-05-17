7cc313e4c08d74676e6784ba4902622106eb86b3
joseungjin <jn5947@naver.com>
2022-05-17 23:32:47 +0900

    Main modify

--- src/componets/Main/Main.js
+++ src/componets/Main/Main.js
@@ -22,8 +22,6 @@ function Main() {
     //   }
     // }
     return (
-      // <div style={{display:'flex', justifyContent:'center', alignItems:'center',width:'100%', height:'100vh'}}>
-       
         <div>
            <SearchBar></SearchBar>
       </div>
