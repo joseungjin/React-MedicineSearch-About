d6d47f4975226c328b6e86df5f207e860e8f2cea
JoSeungJin <82432906+joseungjin@users.noreply.github.com>
2022-06-28 16:22:22 +0900

    Update DetailView.js
    map add

--- src/componets/DetailView/DetailView.js
+++ src/componets/DetailView/DetailView.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useLocation } from "react-router";
+import {API_URL} from "../config";
 
-export const DetailView = () => {
+function DetailView() {
+  const [Medicine, setMedicine] = useState([])
   const { state } = useLocation();
-  console.log(state);
+
+ 
+ const endpoint =`${API_URL}${state}&type=json`;
+
+ 
+  fetch(endpoint)
+  .then(response => response.json())
+  .then(response => {
+    //console.log(response.body)
+    if(response.body.items){
+      setMedicine(response.body.items);
+    }
+  })
+
+  const renderResults = Medicine.map((name,index) => {
+    return (
+      <div key = {index}>
+        {name.itemName}
+        <br/>
+        <img src={name.itemImage} style={{ width: '100%' }}></img>
+        <br/>
+        {name.entpName}
+        <br/>
+        {name.efcyQesitm}
+        <br/>
+        {name.useMethodQesitm}
+        <br/>
+        {name.atpnWarnQesitm}
+        <br/>
+        {name.atpnQesitm}
+        <br/>
+      </div>
+    );
+  });
   return (
-    <div> {state}</div>
+    <div> {renderResults}</div>
   )
 }
+export default DetailView
