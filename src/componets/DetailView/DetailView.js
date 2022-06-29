e6c3f9786273b37afe5a2cb6db1f62d431dd091f
JoSeungJin <82432906+joseungjin@users.noreply.github.com>
2022-06-29 15:24:44 +0900

    Update DetailView.js
    DetilView modify

--- src/componets/DetailView/DetailView.js
+++ src/componets/DetailView/DetailView.js
@@ -1,46 +1,29 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { useLocation } from "react-router";
 import {API_URL} from "../config";
 
 function DetailView() {
-  const [Medicine, setMedicine] = useState([])
   const { state } = useLocation();
 
- 
- const endpoint =`${API_URL}${state}&type=json`;
-
- 
-  fetch(endpoint)
-  .then(response => response.json())
-  .then(response => {
-    //console.log(response.body)
-    if(response.body.items){
-      setMedicine(response.body.items);
-    }
-  })
-
-  const renderResults = Medicine.map((name,index) => {
-    return (
-      <div key = {index}>
-        {name.itemName}
-        <br/>
-        <img src={name.itemImage} style={{ width: '100%' }}></img>
-        <br/>
-        {name.entpName}
-        <br/>
-        {name.efcyQesitm}
-        <br/>
-        {name.useMethodQesitm}
-        <br/>
-        {name.atpnWarnQesitm}
-        <br/>
-        {name.atpnQesitm}
-        <br/>
-      </div>
-    );
-  });
   return (
-    <div> {renderResults}</div>
+    <div>
+    {state.itemName}
+    <br/>
+    <img src={state.itemImage} style={{ width: '100%' }}></img>
+    <br/>
+    {state.entpName}
+    <br/>
+    {state.efcyQesitm}
+    <br/>
+    {state.useMethodQesitm}
+    <br/>
+    {state.atpnWarnQesitm}
+    <br/>
+    {state.atpnQesitm}
+    <br/>
+  </div>
   )
 }
 export default DetailView
+
+
